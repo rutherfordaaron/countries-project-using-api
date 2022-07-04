c77ce5b5989d2022e69643e985267ac55c7823ff
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-04 13:24:00 -0700

    Set up for project
    - Removed unnecessary files
    - Gutted what files I do need to build them from the ground up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  useEffect(() => localStorage.setItem('theme', theme));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app' data-theme={theme}>
+      <header>
+        <h1>Where in the world?</h1>
+        <div className='themeSwitch'>
+
+        </div>
       </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
