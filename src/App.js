e2d97cd87e38850c3a0b82b6010fc075a78bca72
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-04 17:38:15 -0700

    Bug fix
    - Fixed bug where an error would occur if you tried to search an empty string
    - Added a button that will be used to allow filtering by region

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Tile from './components/Tile';
 
 import sunIcon from './icons/sun-solid.svg';
 import moonIcon from './icons/moon-solid.svg';
+import searchIcon from './icons/magnifying-glass-solid.svg';
+import downIcon from './icons/chevron-down-solid.svg';
 
 import './index.css';
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
   useEffect(() => localStorage.setItem('theme', theme));
+
+  const [search, setSearch] = useState('');
+  const [region, setRegion] = useState('');
 
   const themeSwitch = () => {
     if (theme === 'light') {
@@ -17,6 +22,21 @@ const App = () => {
     } else {
       setTheme('light');
       document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
+  const searchChange = (e) => {
+    if (e.which === 13) {
+      if (e.target.value !== '') {
+        let temp = e.target.value.toLowerCase().split(' ');
+        for (let i = 0; i < temp.length; i++) {
+          temp[i] = temp[i][0].toUpperCase() + temp[i].slice(1);
+        }
+        setSearch(temp);
+        e.target.value = temp;
+      } else {
+        setSearch('');
+      }
     }
   }
 
@@ -50,10 +70,15 @@ const App = () => {
       </header>
       <main>
         <div className='search'>
-
+          <img className='filter' src={searchIcon} alt='' />
+          <input id='searchInput' onKeyDown={searchChange} type='text' placeholder='Search for a country...' />
         </div>
+        <button className='regionFilter'>
+          <p>Filter by Region</p>
+          <img src={downIcon} className='filter' alt='' />
+        </button>
         <div className='countryGrid'>
-          {countryTiles.map((element, i) => {
+          {countryTiles.filter(country => country.name.includes(search) && country.region.includes(region)).map((element, i) => {
             return (
               <Tile
                 name={element.name}
